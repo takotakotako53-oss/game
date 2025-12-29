4f733b1bf9669d6b0b8ba998ebd674f13ec69f2a
daisuketakada <takotakotako53@gmail.com>
2025-12-30 02:21:55 +0900

    add analytics

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import Calendar from './calendar';
 import './style.css';
 
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
-  root.render(<Calendar />);
+  root.render(
+    <>
+      <Calendar />
+      <Analytics />
+    </>
+  );
 }
 
